47fd052974579a678648a0af4903ce164d786cd0
Opeyemi Gbadamosi <gbadamosiopeyemi99@gmail.com>
2020-03-29 19:44:43 +0100

    push first code commit

---
+++ src/test/java/NamecheapSignUp/NamecheapSignupTests.java
@@ -0,0 +1,42 @@
+package NamecheapSignUp;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class NamecheapSignupTests {
+
+    private WebDriver driver;
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        //fetch URL to automate
+        driver.get("https://www.namecheap.com/myaccount/signup/");
+        Thread.sleep(5000);
+
+        // maximize ta page of your browser while page loads
+        driver.manage().window().maximize();
+
+        // get page title
+        System.out.println(driver.getTitle());
+
+        // make page wait while url fully loads
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_SignupUName")).sendKeys("OpeTestify");
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_SignupPWord")).sendKeys("7BU@great");
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_ConfirmPassword")).sendKeys("7BU@great");
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_FirstName")).sendKeys("Opeyemi");
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_LastName")).sendKeys("Gbadamosi");
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_Email")).sendKeys("henryt1455@gmail.com");
+        driver.findElement(By.id("ctl00_ctl00_ctl00_ctl00_base_content_web_base_content_home_content_page_content_left_ctl02_CreateUserButton")).click();
+    }
+    public static void main(String args[]) throws InterruptedException {
+        NamecheapSignupTests test = new NamecheapSignupTests();
+        test.setUp();
+    }
+
+}
